915ca754d4750ad2d1772965b42ea7582ce14d7d
Brandon Franks <captainxcuddles@hotmail.com>
2022-06-29 23:57:32 -0600

    adding notes

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const PORT = 3001;
 const path = require('path')
 
 
-
+//routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/assets/index.html'));
 });
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '.public/assets/notes.html'));
 });
-
+//listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 }) 
