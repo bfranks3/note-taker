06a4f5c2422d0fb73451d4d8ba4f97192747c90f
Brandon Franks <captainxcuddles@hotmail.com>
2022-06-30 00:40:30 -0600

    updating README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Note Taker Starter Code
+# Note Taker 
+
+# Description
+This is an app to write and save ntoes and can orgainze thoughts and keep track of tasks when they need to be completed Technologies
+
+# Contact
+
+# Links
+GitHub: https://github.com/bfranks3/note-taker
+# Technologies Used
+HTML, CSS, JavaScript, Node.js, Express.js
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 
 //routes
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../../public/assets/index.html"))
 });
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../../public/assets/notes.html"))
 });
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/index.html"))
+    res.sendFile(path.join(__dirname, "../../public/assets/index.html"))
 });
 
 module.exports = router;
